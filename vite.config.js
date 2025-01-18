e7e3c7c92953f961212b9fce1e4692c68933249e
claire35510 <123732233+claire35510@users.noreply.github.com>
2025-01-18 21:21:36 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    plugins: [svelte()]
+  plugins: [svelte()],
+  resolve: {
+    dedupe: ['svelte'] // Évite les conflits entre différentes versions de Svelte
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020', // Pour supporter les modules modernes
+    }
+  }
 });
